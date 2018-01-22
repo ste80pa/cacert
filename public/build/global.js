9c7710fad39469378cd5bf85f00d52913d663edc
ste80pa@gmail.com <ste80pa@gmail.com>
2018-01-22 10:12:29 +0100

    Initial Commit

---
+++ public/build/global.js
@@ -0,0 +1,28 @@
+webpackJsonp([2],{
+
+/***/ "./assets/css/global.scss":
+/*!********************************!*\
+  !*** ./assets/css/global.scss ***!
+  \********************************/
+/*! dynamic exports provided */
+/*! all exports used */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "./assets/js/global.js":
+/*!*****************************!*\
+  !*** ./assets/js/global.js ***!
+  \*****************************/
+/*! dynamic exports provided */
+/*! all exports used */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! ../css/global.scss */ "./assets/css/global.scss");
+
+/***/ })
+
+},["./assets/js/global.js"]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hc3NldHMvY3NzL2dsb2JhbC5zY3NzP2JjYmUiLCJ3ZWJwYWNrOi8vLy4vYXNzZXRzL2pzL2dsb2JhbC5qcyJdLCJuYW1lcyI6WyJyZXF1aXJlIl0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7O0FBQUEseUM7Ozs7Ozs7Ozs7OztBQ0FBLG1CQUFBQSxDQUFRLG9EQUFSLEUiLCJmaWxlIjoiZ2xvYmFsLmpzIiwic291cmNlc0NvbnRlbnQiOlsiLy8gcmVtb3ZlZCBieSBleHRyYWN0LXRleHQtd2VicGFjay1wbHVnaW5cblxuXG4vLy8vLy8vLy8vLy8vLy8vLy9cbi8vIFdFQlBBQ0sgRk9PVEVSXG4vLyAuL2Fzc2V0cy9jc3MvZ2xvYmFsLnNjc3Ncbi8vIG1vZHVsZSBpZCA9IC4vYXNzZXRzL2Nzcy9nbG9iYWwuc2Nzc1xuLy8gbW9kdWxlIGNodW5rcyA9IDIiLCJyZXF1aXJlKCcuLi9jc3MvZ2xvYmFsLnNjc3MnKTtcblxuXG5cbi8vIFdFQlBBQ0sgRk9PVEVSIC8vXG4vLyAuL2Fzc2V0cy9qcy9nbG9iYWwuanMiXSwic291cmNlUm9vdCI6IiJ9
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+// webpack.config.js
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    // the project directory where all compiled assets will be stored
+    .setOutputPath('public/build/')
+
+    // the public path used by the web server to access the previous directory
+    .setPublicPath('/build')
+
+    // will create public/build/app.js and public/build/app.css
+   .addEntry('app', './assets/js/app.js')
+   .addEntry('global', './assets/js/global.js')
+  
+   
+   // .addEntry('page1', 'assets/js/page1.js')
+   // .addEntry('page2', 'assets/js/page2.js')
+
+    // this creates a 'vendor.js' file with jquery and the bootstrap JS module
+    // these modules will *not* be included in page1.js or page2.js anymore
+    .createSharedEntry('vendor', [
+        'jquery',
+        'bootstrap',
+
+        // you can also extract CSS - this will create a 'vendor.css' file
+        // this CSS will *not* be included in page1.css or page2.css anymore
+        
+       // 'bootstrap-sass/assets/stylesheets/_bootstrap.scss'
+    ])
+
+    // allow sass/scss files to be processed
+   .enableSassLoader(function(sassOptions) {}, {
+         resolveUrlLoader: false
+     })
+    // allow legacy applications to use $/jQuery as a global variable
+    .autoProvidejQuery()
+
+    .enableSourceMaps(!Encore.isProduction())
+
+    // empty the outputPath dir before each build
+    .cleanupOutputBeforeBuild()
+
+    // show OS notifications when builds finish/fail
+    .enableBuildNotifications()
+
+    // create hashed filenames (e.g. app.abc123.css)
+    // .enableVersioning()
+;
+
+// export the final configuration
+module.exports = Encore.getWebpackConfig();
